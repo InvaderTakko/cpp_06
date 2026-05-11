a016bb3f2d6e125029da485e56fb582d9b653f19
Stefan Ruff <sruff@2-E-3.42heilbronn.de>
2026-05-11 18:50:59 +0200

    format_and_print function is somewhat complete. not tested yet

--- scala.cpp
+++ scala.cpp
@@ -6,7 +6,7 @@
 /*   By: sruff <sruff@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/08 15:21:17 by sruff             #+#    #+#             */
-/*   Updated: 2026/05/11 16:49:19 by sruff            ###   ########.fr       */
+/*   Updated: 2026/05/11 18:49:09 by sruff            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 #include <cstdint>
 #include <cstdlib>
 #include <iomanip>
+#include <ios>
 #include <iostream>
 #include <limits>
 #include <ostream>
@@ -35,10 +36,10 @@ ScalarConverter::~ScalarConverter()
 {
 }
 
-static void format_and_print(char c, int32_t i, float f, double d, char typeInfo)
+static void	format_and_print(char c, int32_t i, float f, double d,
+		char typeInfo)
 {
 	// char
-
 	std::cout << "char: ";
 	if (std::isinf(f) || std::isnan(f)) //
 		std::cout << "impossible" << std::endl;
@@ -46,13 +47,23 @@ static void format_and_print(char c, int32_t i, float f, double d, char typeInfo
 		std::cout << "impossible" << std::endl;
 	else if (c > 126 || c < 32)
 		std::cout << "none displayable" << std::endl;
-	
+	else
+		std::cout << c << std::endl;
 	// int
 	std::cout << "int: ";
-	if ()
+	if (i < std::numeric_limits<int>::min()
+		|| i > std::numeric_limits<int>::max())
+		std::cout << "impossibel" << std::endl;
+	else
+		std::cout << i << std::endl;
 	// float
+	if (typeInfo == 'f')
+	{
+		std::cout << "float: " << std::fixed << std::setprecision(1) << f << "f\n";
+	}
+	else if (typeInfo == 'd')
+		std::cout << "double: " << std::fixed << std::setprecision(1) << std::endl;
 	// double
-	
 }
 
 void ScalarConverter::convert(const std::string &str)
@@ -66,7 +77,7 @@ void ScalarConverter::convert(const std::string &str)
 		return ;
 	}
 
-	try			// int
+	try // int
 	{
 		int64_t l = std::strtol(str.c_str(), &end, 10);
 		if (*end == '\0')
@@ -80,41 +91,41 @@ void ScalarConverter::convert(const std::string &str)
 	}
 	catch (...)
 	{
-		
 	}
-	
-	try			//float
+
+	try // float
 	{
 		d = std::strtod(str.c_str(), &end);
 		if (*end == 'f' && *(end + 1) == '\0')
 		{
-			if (d >std::numeric_limits<float>::max() || d < std::numeric_limits<float>::min())
-				throw std::out_of_range("float over/underflow")
-			f =  static_cast<float>(d);
+			if (d > std::numeric_limits<float>::max()
+				|| d < std::numeric_limits<float>::min())
+				throw std::out_of_range("float over/underflow");
+			f = static_cast<float>(d);
 			std::cout << f << std::endl;
+			// format_and_print(char c, int32_t i, float f, double d,
+				char typeInfo)
 			return ;
 		}
-		throw
-		{
-			std::invalid_argument("not a float")
-		}
-		catch (...) {}
-		
+		throw std::invalid_argument("not a float");
 	}
-	
-	try			//double
+	catch (...)
+	{
+	}
+	try // double
 	{
 		d = std::strtod(str.c_str(), &end);
 		if (*end == '\0')
 		{
-			std::cout <<  d << std::endl;
+			std::cout << d << std::endl;
 			return ;
 		}
-		throw std::invalid_argument("not a double");	
-		
+		throw std::invalid_argument("not a double");
 	}
-	catch (...) {}
-	
+	catch (...)
+	{
+	}
+
 	std::cout << "char: impossible\n";
 	std::cout << "int: impossible\n";
 	std::cout << "float: impossible\n";
