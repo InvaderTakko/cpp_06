875d438ff6c30dff27db2a68549d8cbc6a6c7ac5
Stefan Ruff <sruff@2-E-4.42heilbronn.de>
2026-05-11 14:44:24 +0200

    implementing scala float

--- scala.cpp
+++ scala.cpp
@@ -6,7 +6,7 @@
 /*   By: sruff <sruff@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/08 15:21:17 by sruff             #+#    #+#             */
-/*   Updated: 2026/05/10 17:06:49 by sruff            ###   ########.fr       */
+/*   Updated: 2026/05/11 14:43:37 by sruff            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,13 +38,15 @@ ScalarConverter::~ScalarConverter()
 void ScalarConverter::convert(const std::string &str)
 {
 	char *end;
+	float f;
+	double d; // stdint equivalent only in c++23
 	if (!std::isdigit(str[0]) && str.length() == 1)
 	{
 		std::cout << static_cast<int>(str[0]) << std::endl;
 		return ;
 	}
 
-	try
+	try			// int
 	{
 		int64_t l = std::strtol(str.c_str(), &end, 10);
 		if (*end == '\0')
@@ -53,9 +55,24 @@ void ScalarConverter::convert(const std::string &str)
 				|| l > std::numeric_limits<int>::max())
 				throw std::out_of_range("int under/overflow");
 			std::cout << static_cast<int>(l) << std::endl;
+			return ;
 		}
 	}
 	catch (...)
 	{
+		
+	}
+	
+	try			//float
+	{
+		d = std::strtod(str.c_str(), &end);
+		if (*end == 'f' && *(end + 1) == '\0')
+		{
+			f =  static_cast<float>(d);
+			std::cout << f << std::endl;
+			return ;
+		}
+		throw
+		
 	}
 }
