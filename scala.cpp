1e453d4d22ee20e2d05b3f09ad105bf7fe139d99
Stefan Ruff <sruff@2-E-9.42heilbronn.de>
2026-05-12 12:12:04 +0200

    printing and recognition logic kinda works now

--- scala.cpp
+++ scala.cpp
@@ -6,7 +6,7 @@
 /*   By: sruff <sruff@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/08 15:21:17 by sruff             #+#    #+#             */
-/*   Updated: 2026/05/11 18:49:09 by sruff            ###   ########.fr       */
+/*   Updated: 2026/05/12 12:09:14 by sruff            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,6 +39,7 @@ ScalarConverter::~ScalarConverter()
 static void	format_and_print(char c, int32_t i, float f, double d,
 		char typeInfo)
 {
+	(void)typeInfo;
 	// char
 	std::cout << "char: ";
 	if (std::isinf(f) || std::isnan(f)) //
@@ -57,18 +58,16 @@ static void	format_and_print(char c, int32_t i, float f, double d,
 	else
 		std::cout << i << std::endl;
 	// float
-	if (typeInfo == 'f')
-	{
-		std::cout << "float: " << std::fixed << std::setprecision(1) << f << "f\n";
-	}
-	else if (typeInfo == 'd')
-		std::cout << "double: " << std::fixed << std::setprecision(1) << std::endl;
+	std::cout << "float: " << std::fixed << std::setprecision(1) << f << "f\n";
 	// double
+	std::cout << "double: " << std::fixed << std::setprecision(1) << d << std::endl;
 }
 
 void ScalarConverter::convert(const std::string &str)
 {
 	char *end;
+	//char	c;
+	int64_t l;
 	float f;
 	double d; // stdint equivalent only in c++23
 	if (!std::isdigit(str[0]) && str.length() == 1)
@@ -79,7 +78,7 @@ void ScalarConverter::convert(const std::string &str)
 
 	try // int
 	{
-		int64_t l = std::strtol(str.c_str(), &end, 10);
+		l = std::strtol(str.c_str(), &end, 10);
 		if (*end == '\0')
 		{
 			if (l < std::numeric_limits<int>::min()
@@ -103,8 +102,8 @@ void ScalarConverter::convert(const std::string &str)
 				throw std::out_of_range("float over/underflow");
 			f = static_cast<float>(d);
 			std::cout << f << std::endl;
-			// format_and_print(char c, int32_t i, float f, double d,
-				char typeInfo)
+			format_and_print(static_cast<int>(str[0]),  static_cast<int>(l),  f,  d,
+				't');
 			return ;
 		}
 		throw std::invalid_argument("not a float");
@@ -117,7 +116,9 @@ void ScalarConverter::convert(const std::string &str)
 		d = std::strtod(str.c_str(), &end);
 		if (*end == '\0')
 		{
-			std::cout << d << std::endl;
+			//std::cout << d << std::endl;
+			format_and_print(static_cast<int>(str[0]),  static_cast<int>(l),  static_cast<float>(d),  d,
+				't');
 			return ;
 		}
 		throw std::invalid_argument("not a double");
@@ -125,7 +126,7 @@ void ScalarConverter::convert(const std::string &str)
 	catch (...)
 	{
 	}
-
+	// if no check applies
 	std::cout << "char: impossible\n";
 	std::cout << "int: impossible\n";
 	std::cout << "float: impossible\n";
