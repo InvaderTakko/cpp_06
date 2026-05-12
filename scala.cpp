0524132a197f76f0af982e31f3f7892892d26fb2
Stefan Ruff <sruff@2-E-9.42heilbronn.de>
2026-05-12 12:48:46 +0200

    works now needs some minor edgecase fixes

--- scala.cpp
+++ scala.cpp
@@ -6,7 +6,7 @@
 /*   By: sruff <sruff@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/08 15:21:17 by sruff             #+#    #+#             */
-/*   Updated: 2026/05/12 12:09:14 by sruff            ###   ########.fr       */
+/*   Updated: 2026/05/12 12:43:08 by sruff            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -52,9 +52,10 @@ static void	format_and_print(char c, int32_t i, float f, double d,
 		std::cout << c << std::endl;
 	// int
 	std::cout << "int: ";
-	if (i < std::numeric_limits<int>::min()
-		|| i > std::numeric_limits<int>::max())
-		std::cout << "impossibel" << std::endl;
+	if (std::isnan(f) || std::isinf(f))
+		std::cout << "impossible" << std::endl;
+	else if (d < std::numeric_limits<int>::min() || d > std::numeric_limits<int>::max())
+		std::cout << "impossible" << std::endl;
 	else
 		std::cout << i << std::endl;
 	// float
@@ -70,9 +71,10 @@ void ScalarConverter::convert(const std::string &str)
 	int64_t l;
 	float f;
 	double d; // stdint equivalent only in c++23
-	if (!std::isdigit(str[0]) && str.length() == 1)
+	if (!std::isdigit(str[0]) && str.length() == 1 && str[0] != '-' && str[0] != '+')
 	{
-		std::cout << static_cast<int>(str[0]) << std::endl;
+		//std::cout << static_cast<int>(str[0]) << std::endl;
+		format_and_print(str[0],  static_cast<int>(str[0]),  static_cast<float>(str[0]),static_cast<double>(str[0]),'t');
 		return ;
 	}
 
@@ -84,7 +86,8 @@ void ScalarConverter::convert(const std::string &str)
 			if (l < std::numeric_limits<int>::min()
 				|| l > std::numeric_limits<int>::max())
 				throw std::out_of_range("int under/overflow");
-			std::cout << static_cast<int>(l) << std::endl;
+			//std::cout << static_cast<int>(l) << std::endl;
+			format_and_print(static_cast<int>(l),  static_cast<int>(l),  static_cast<float>(l),static_cast<double>(l),'t');
 			return ;
 		}
 	}
@@ -97,12 +100,11 @@ void ScalarConverter::convert(const std::string &str)
 		d = std::strtod(str.c_str(), &end);
 		if (*end == 'f' && *(end + 1) == '\0')
 		{
-			if (d > std::numeric_limits<float>::max()
-				|| d < std::numeric_limits<float>::min())
+if (!std::isinf(d) && !std::isnan(d) && (d > std::numeric_limits<float>::max() || d < std::numeric_limits<float>::min()))
 				throw std::out_of_range("float over/underflow");
 			f = static_cast<float>(d);
-			std::cout << f << std::endl;
-			format_and_print(static_cast<int>(str[0]),  static_cast<int>(l),  f,  d,
+			//std::cout << f << std::endl;
+			format_and_print(static_cast<char>(f),  static_cast<int>(f),  f,  static_cast<double>(d),
 				't');
 			return ;
 		}
@@ -117,7 +119,7 @@ void ScalarConverter::convert(const std::string &str)
 		if (*end == '\0')
 		{
 			//std::cout << d << std::endl;
-			format_and_print(static_cast<int>(str[0]),  static_cast<int>(l),  static_cast<float>(d),  d,
+			format_and_print(static_cast<char>(d),  static_cast<int>(d),  static_cast<float>(d),  d,
 				't');
 			return ;
 		}
